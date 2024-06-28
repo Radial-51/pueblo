e62d420c5f6e58d3043524bbafc413ff0c24c857
NATHAN HERNANDEZ MORENO <133732798+NATHANHERNANDEZ14@users.noreply.github.com>
2024-06-27 19:58:17 -0600

    js
    Vinculo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,6 +50,7 @@
     {{-- Add here extra stylesheets --}}
     <link rel="stylesheet" href="{{ secure_asset('vendor/adminlte/dist/css/adminlte.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/styles-home.css') }}">
+    <link rel="stylesheet" href="{{  secure_asset('vendor/adminlte/dist/js/adminlte.js')}}">
 
 @stop
 
