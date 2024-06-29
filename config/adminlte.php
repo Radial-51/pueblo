d2416d4582cd65022b8d8ec4218a0f48fcfdf8da
NATHAN HERNANDEZ MORENO <133732798+NATHANHERNANDEZ14@users.noreply.github.com>
2024-06-28 23:35:19 -0600

    d
    d

--- config/adminlte.php
+++ config/adminlte.php
@@ -455,7 +455,7 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
