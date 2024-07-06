72f23081706872154e059da3058f4083c08a507c
NATHAN HERNANDEZ MORENO <133732798+NATHANHERNANDEZ14@users.noreply.github.com>
2024-07-06 11:10:55 -0600

    sd
    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <!doctype html>
 <html lang="en">
+    @component('navegacion')
+    @component('cuadricula')
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
